7759c34ebf5c7df23c844f94bb97f38d141b2c0d
Benedikt Burger <67148916+bmoneke@users.noreply.github.com>
2023-11-06 14:12:17 +0100

    Fix type errors and numpy

--- tests/management/test_data_logger.py
+++ tests/management/test_data_logger.py
@@ -22,9 +22,9 @@
 # THE SOFTWARE.
 #
 
+from math import isnan
 from unittest.mock import MagicMock
 
-import numpy as np
 import pytest
 
 from pyleco.management.data_logger import DataLogger, nan, ValuingModes
@@ -33,8 +33,8 @@ from pyleco.management.data_logger import DataLogger, nan, ValuingModes
 @pytest.fixture
 def data_logger() -> DataLogger:
     dl = DataLogger()
-    dl.subscribe = MagicMock()
-    dl.unsubscribe_all = MagicMock()
+    dl.subscribe = MagicMock()  # type: ignore[method-assign]
+    dl.unsubscribe_all = MagicMock()  # type: ignore[method-assign]
     dl.start_collecting(
         subscriptions=["time", "test", "2"],
         trigger="test",
@@ -46,13 +46,13 @@ def data_logger() -> DataLogger:
 
 
 def _setup_listen_does_not_start_collecting_without_start_data(data_logger: DataLogger):
-    data_logger.start_collecting = MagicMock()
+    data_logger.start_collecting = MagicMock()  # type: ignore[method-assign]
     data_logger._listen_setup()
     data_logger.start_collecting.assert_not_called()
 
 
 def _setup_listen_starts_collecting(data_logger: DataLogger):
-    data_logger.start_collecting = MagicMock()
+    data_logger.start_collecting = MagicMock()  # type: ignore[method-assign]
     data_logger._listen_setup(start_data={"2": 7})
     data_logger.start_collecting.assert_called_once_with(start_data={"2": 7})
 
@@ -65,7 +65,7 @@ def test_handle_subscription_data_without_trigger(data_logger: DataLogger):
 
 
 def test_handle_subscription_data_triggers(data_logger: DataLogger):
-    data_logger.make_data_point = MagicMock()
+    data_logger.make_data_point = MagicMock()  # type: ignore[method-assign]
     data_logger.handle_subscription_data({"test": 5})
     data_logger.make_data_point.assert_called_once()
 
@@ -115,7 +115,7 @@ class Test_calculate_single_data:
         assert data_logger.calculate_single_data("2", tmp=list) == result
 
     def test_average_results_in_nan(self, data_logger: DataLogger):
-        assert np.isnan(data_logger.calculate_single_data("2", tmp=[]))
+        assert isnan(data_logger.calculate_single_data("2", tmp=[]))
 
     @pytest.mark.parametrize("list, result", (
             ([2, 3], 3),
@@ -127,7 +127,7 @@ class Test_calculate_single_data:
 
     def test_last_results_in_nan(self, data_logger: DataLogger):
         data_logger.valuing = data_logger.last
-        assert np.isnan(data_logger.calculate_single_data("2", tmp=[]))
+        assert isnan(data_logger.calculate_single_data("2", tmp=[]))
 
     @pytest.mark.parametrize("list, result", (
             ([2, 3], 2.5),
@@ -151,7 +151,7 @@ class Test_calculate_single_data:
     def test_reapeating_without_last_value_results_in_nan(self, data_logger: DataLogger):
         data_logger.value_repeating = True
         data_logger.lists["2"] = []
-        assert np.isnan(data_logger.calculate_single_data("2", tmp=[]))
+        assert isnan(data_logger.calculate_single_data("2", tmp=[]))
 
 
 class Test_last:
@@ -159,4 +159,4 @@ class Test_last:
         assert data_logger.last([1, 2, 3, 4, 5]) == 5
 
     def test_empty_list_returns_nan(self, data_logger: DataLogger):
-        assert np.isnan(data_logger.last([]))
+        assert isnan(data_logger.last([]))
