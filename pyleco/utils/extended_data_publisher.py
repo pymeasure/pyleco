891d80380fc213060287459c169b10dd3bf81759
Benedikt Burger <67148916+BenediktBurger@users.noreply.github.com>
2024-07-05 18:35:07 +0200

    Add extended data publisher

---
+++ pyleco/utils/extended_data_publisher.py
@@ -0,0 +1,68 @@
+#
+# This file is part of the PyLECO package.
+#
+# Copyright (c) 2023-2024 PyLECO Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from __future__ import annotations
+from typing import Any, Callable, Generator, Union
+
+from ..core.message import Message, MessageTypes
+from ..core.data_message import DataMessage
+from ..json_utils.rpc_generator import RPCGenerator
+from .data_publisher import DataPublisher
+
+class ExtendedDataPublisher(DataPublisher):
+    """A DataPublisher, which sends the data also via the control protocol."""
+
+    def __init__(
+        self, full_name: str, send_message_method: Callable[[Message], None], **kwargs
+    ) -> None:
+        super().__init__(full_name, **kwargs)
+        self.send_control_message = send_message_method
+        self.subscribers: set[bytes] = set()
+        self.rpc_generator = RPCGenerator()
+
+    def register_subscriber(self, subscriber: Union[bytes, str]) -> None:
+        """Register a subscriber, that it may receive messages via data_protocol."""
+        if isinstance(subscriber, str):
+            subscriber = subscriber.encode()
+        self.subscribers.add(subscriber)
+
+    def convert_data_message_to_messages(
+        self, data_message: DataMessage, receivers: set[Union[bytes, str]],
+    ) -> Generator[Message, Any, Any]:
+        cid = data_message.conversation_id
+        data = self.rpc_generator.build_request_str(method="set_subscription_message")
+        for receiver in receivers:
+            yield Message(
+                receiver=receiver,
+                data=data,
+                conversation_id=cid,
+                additional_payload=data_message.payload,
+                message_type=MessageTypes.JSON,
+            )
+
+    def send_message(self, message: DataMessage) -> None:
+        super().send_message(message)
+        for msg in self.convert_data_message_to_messages(message, self.subscribers):
+            # ideas: change to ask and check, whether it succeeded, otherwise remove subscriber
+            self.send_control_message(msg)
---
+++ tests/utils/test_extended_data_publisher.py
@@ -0,0 +1,97 @@
+#
+# This file is part of the PyLECO package.
+#
+# Copyright (c) 2023-2024 PyLECO Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import pytest
+
+from pyleco.test import FakeContext
+from pyleco.utils.extended_data_publisher import ExtendedDataPublisher, Message, DataMessage, MessageTypes
+
+
+CID = b"conversation_id;"
+
+
+@pytest.fixture
+def fake_send_message():
+    global messages
+    messages = []
+    def _fsm(message: Message):
+        global messages
+        messages.append(message)
+    return _fsm
+
+
+@pytest.fixture
+def publisher(fake_send_message) -> ExtendedDataPublisher:
+    publisher = ExtendedDataPublisher(
+        "fn", send_message_method=fake_send_message,
+        context=FakeContext(),  # type: ignore
+    )
+    return publisher
+
+
+@pytest.fixture
+def data_message() -> DataMessage:
+    return DataMessage(
+            topic="topic", conversation_id=CID, data=b"0", additional_payload=[b"1", b"2"]
+        )
+
+
+def test_register_subscribers(publisher: ExtendedDataPublisher):
+    # act
+    publisher.register_subscriber("abcdef")
+    assert b"abcdef" in publisher.subscribers
+
+    publisher.register_subscriber(b"ghi")
+    assert b"ghi" in publisher.subscribers
+
+
+@pytest.mark.parametrize("receivers", (set(), {b"abc"}, {b"abc", b"def"}, {"string"}))
+def test_convert(publisher: ExtendedDataPublisher, receivers, data_message: DataMessage):
+    msgs = publisher.convert_data_message_to_messages(data_message, receivers=receivers)
+    for rec, msg in zip(receivers, msgs, strict=True):
+        assert msg == Message(
+            receiver=rec,
+            data={"id": 1, "method": "set_subscription_message", "jsonrpc": "2.0"},
+            conversation_id=CID,
+            message_type=MessageTypes.JSON,
+            additional_payload=data_message.payload,
+        )
+
+
+def test_send_message(publisher: ExtendedDataPublisher, data_message: DataMessage):
+    # arrange
+    publisher.register_subscriber("abc")
+    # act
+    publisher.send_message(data_message)
+    assert publisher.socket._s == [data_message.to_frames()]
+    global messages
+    assert messages == [
+        Message(
+            "abc",
+            data={"id": 1, "method": "set_subscription_message", "jsonrpc": "2.0"},
+            conversation_id=CID,
+            message_type=MessageTypes.JSON,
+            additional_payload=data_message.payload,
+        )
+    ]
